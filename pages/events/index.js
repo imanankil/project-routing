61f92fe67ca96f5673c794464b2a306b2fdcfb18
ianmanankil <imanankil@academic.rrc.ca>
2024-02-05 09:28:54 -0600

    Adding main pages and routes 154

---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+function AllEventsPage () {
+    return (
+        <div>
+            <h1>
+                All Events
+            </h1>
+        </div>
+    );
+
+}
+
+export default AllEventsPage;
