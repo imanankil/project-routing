ed04598880c524c4ce40e68204c13cf663f1549b
ianmanankil <imanankil@academic.rrc.ca>
2024-02-05 10:40:18 -0600

    Adding More react components and connection components 157

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -3,10 +3,20 @@ import EventItem from "./event-items";
 function EventList(props) {
     const { items } = props;
     
-    return <ul>
-        {items.map(event => <EventItem/>)}
+    return ( 
+    <ul>
+        {items.map(event => (
+        <EventItem 
+        key={event.id}
+        id={event.id} 
+        title={event.title} 
+        location={event.location} 
+        date={event.date} 
+        image={event.image}
+        />
+    ))}
      </ul>
-    
+    );
 }
 
 export default EventList;
